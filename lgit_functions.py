ade7a615ca3935377f3ca1fe66faa6c333ee6ad4
luan <luannguyen062@gmail.com)>
2019-03-19 22:20:16 +0700

    in the middle of writing find_file_in_index

--- lgit_functions.py
+++ lgit_functions.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-from os import mkdir, environ, path
+from os import mkdir, environ, path, O_RDONLY, O_RDWR, O_CREAT
 from utility import try_and_pass_function
 
 
@@ -12,7 +12,7 @@ def init_lgit(args):
     # Create objects directory
     try_and_pass_function(mkdir, FileExistsError, "./.lgit/objects")
     # Create commit directory
-    try_and_pass_function(mkdir, FileExistsError, "./.lgit/commmits")
+    try_and_pass_function(mkdir, FileExistsError, "./.lgit/commits")
     # Create snapshots directory
     try_and_pass_function(mkdir, FileExistsError, "./.lgit/snapshots")
     # Create index file
@@ -25,39 +25,103 @@ def init_lgit(args):
     config.close()
 
 
-def check_lgit_directory(args):
+def check_lgit_directory():
     """
     Check if the working directory has a lgit directory in it or not.
     """
-    return path.exist("./.lgit")
+    path = handle_file_path(".")
+    while True:
+        if path.exists(path + "/.lgit"):
+            return True
+        elif path == "/":
+            break
+        else:
+            path = path.split(path)[0]
+    return False
 
 
 def add_lgit(args):
-    if not check_lgit_directory:
+    if not check_lgit_directory():
+        print("fatal: not a git directory (or any of the parentdirectories)")
+        return
+    path_list = [handle_path(name) for name in args.filenames[::-1]]
+    while path_list:
+        current_path = path_list.pop()
+        if path.isfile(current_path):
+            add_file(current_path)
+        else:
+            add_directory(current_path, path_list)
+    pass
+
+
+def add_file(current_path):
+    pass
+
+
+def find_file_in_index(filename):
+    """
+    Find the line that lists the filename in an index file
+
+    Input:
+        - filename: the name of the file
+
+    Output:
+        - descriptor: a file descriptor at the start of the line that has the
+          name of the file. Will be none if such line doesn't exist.
+        - success: True if the function runs normally. False if something goes
+          wrong
+    """
+    try:
+        file = open("./.lgit/index", "r+")
+    except PermissionDenied:
+        descriptor = None
+        print("Cannot open index file: PermissionDenied")
+        return None, False
+    readtext = os.read(descriptor, )
+    pass
+
+
+def add_directory(current_path, path_list):
+    pass
+
+
+def commit_lgit(args):
+    if not check_lgit_directory():
         print("fatal: not a git directory (or any of the parentdirectories)")
         return
     pass
 
 
-def commit_lgit(args):
-    pass
-
-
 def remove_lgit(args):
+    if not check_lgit_directory():
+        print("fatal: not a git directory (or any of the parentdirectories)")
+        return
     pass
 
 
 def config_lgit(args):
+    if not check_lgit_directory():
+        print("fatal: not a git directory (or any of the parentdirectories)")
+        return
     pass
 
 
 def show_status_lgit(args):
+    if not check_lgit_directory():
+        print("fatal: not a git directory (or any of the parentdirectories)")
+        return
     pass
 
 
 def list_files_lgit(args):
+    if not check_lgit_directory():
+        print("fatal: not a git directory (or any of the parentdirectories)")
+        return
     pass
 
 
 def show_log_lgit(args):
+    if not check_lgit_directory():
+        print("fatal: not a git directory (or any of the parentdirectories)")
+        return
     pass
--- utility.py
+++ utility.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+from hashlib import sha1
 
 
 def try_and_pass_function(function, error, *args, **kwargs):
@@ -6,3 +7,30 @@ def try_and_pass_function(function, error, *args, **kwargs):
         function(*args, **kwargs)
     except error:
         pass
+
+
+def handle_path(name, relative=False):
+    """
+    Return an absolute path
+    """
+    if name.startswith("~"):
+        name = path.expanduser(name)
+    if relative:
+        return path.relpath(name)
+    else:
+        return path.abspath(name)
+
+
+def hash_file_content(path, binary=False):
+    hash = sha1()
+    file = open(path, "rb")
+    file_content = file.read()
+    hash.update(file_content)
+    if binary:
+        return hash.digest()
+    else:
+        return hash.hexdigest()
+
+
+def check_permission(filepath):
+    return True
