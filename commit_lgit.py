d33b4611a328fa1f18835ae935b2020d7f39ee7e
luan <luannguyen062@gmail.com)>
2019-03-21 19:30:14 +0700

    test1

--- commit_lgit.py
+++ commit_lgit.py
@@ -7,4 +7,5 @@ def commit_lgit(args, parent_dir):
         config_file = open(config_file_path, "r+")
     except PermissionDenied:
         print("Unable to access", config_file_path)
+        return
     pass
